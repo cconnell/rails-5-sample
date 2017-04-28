f8ed4e7fa74007d0ba486094a0f2c84cf03bf33d
Chris Connell <chris.g.connell@gmail.com>
2017-04-28 13:43:13 -0500

    add activation and tests

--- app/controllers/acccount_activations_controller.rb
+++
@@ -1,2 +0,0 @@
-class AcccountActivationsController < ApplicationController
-end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -13,6 +13,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination', count: 2
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
+      assert user.activated?
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
@@ -27,5 +28,10 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert user.activated?
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
   end
 end
